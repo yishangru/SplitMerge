a673a81b71817f099877ad209dc329b0ab9bc393
Shangru Yi <syi73@gatech.edu>
2021-04-25 00:53:58 -0400

    split merge: add script

--- script.sh
+++ script.sh
@@ -2,14 +2,14 @@ bin/clang -S -emit-llvm -O1 -Xclang -disable-llvm-passes ../llvm/lib/Transforms/
 
 bin/clang -S -emit-llvm ../llvm/lib/Transforms/SplitMerge/test/src/sample1.c -o ../llvm/lib/Transforms/SplitMerge/test/ir/sample1-no.ll
 
-bin/opt -mem2reg ../llvm/lib/Transforms/SplitMerge/test/ir/sample1-no.ll > ../llvm/lib/Transforms/SplitMerge/test/ir/sample1-no-ssa.ll
+bin/opt -S -mem2reg ../llvm/lib/Transforms/SplitMerge/test/ir/sample2-no.ll -o ../llvm/lib/Transforms/SplitMerge/test/ir/sample2-no-ssa.ll
 
-bin/opt -sccp ../llvm/lib/Transforms/SplitMerge/test/ir/sample1-no-ssa.ll > ../llvm/lib/Transforms/SplitMerge/test/ir/sample1-no-ssa-sccp.ll
+bin/opt -S -sccp ../llvm/lib/Transforms/SplitMerge/test/ir/sample2-no-ssa.ll -o ../llvm/lib/Transforms/SplitMerge/test/ir/sample2-no-ssa-sccp.ll
 
 bin/clang -S -emit-llvm -O1 -Xclang -disable-llvm-passes ../llvm/lib/Transforms/SplitMerge/test/src/sample2.c -o ../llvm/lib/Transforms/SplitMerge/test/ir/sample2.ll
 
 bin/clang -S -emit-llvm ../llvm/lib/Transforms/SplitMerge/test/src/sample2.c -o ../llvm/lib/Transforms/SplitMerge/test/ir/sample2-no.ll
 
-bin/opt -mem2reg ../llvm/lib/Transforms/SplitMerge/test/ir/sample2-no.ll > ../llvm/lib/Transforms/SplitMerge/test/ir/sample2-no-ssa.ll
+bin/opt -S -mem2reg ../llvm/lib/Transforms/SplitMerge/test/ir/sample2-no.ll -o ../llvm/lib/Transforms/SplitMerge/test/ir/sample2-no-ssa.ll
 
-bin/opt -sccp ../llvm/lib/Transforms/SplitMerge/test/ir/sample2-no-ssa.ll > ../llvm/lib/Transforms/SplitMerge/test/ir/sample2-no-ssa-sccp.ll
+bin/opt -S -sccp ../llvm/lib/Transforms/SplitMerge/test/ir/sample2-no-ssa.ll -o ../llvm/lib/Transforms/SplitMerge/test/ir/sample2-no-ssa-sccp.ll
